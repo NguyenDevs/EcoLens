000ec88bc8195af699e690f3621d9302351d55b5
Nguyên Devs <144598460+NguyenDevs@users.noreply.github.com>
2025-12-28 15:19:10 +0700

    Clean up: Stop tracking files listed in .gitignore

--- app/build/intermediates/cxx/Debug/281xl291/logs/arm64-v8a/build_command_native-lib.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-"C:\\Users\\ASUS\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
-  -C ^
-  "D:\\Source Code\\EcoLens\\app\\.cxx\\Debug\\281xl291\\arm64-v8a" ^
-  native-lib
