cc2d43496ada33091cfc742fc243c43fe06c950d
Nguyên Devs <144598460+NguyenDevs@users.noreply.github.com>
2026-01-09 15:27:17 +0700

    Update MainActivity.kt

--- app/src/main/java/com/nguyendevs/ecolens/MainActivity.kt
+++ app/src/main/java/com/nguyendevs/ecolens/MainActivity.kt
@@ -27,8 +27,8 @@ import com.bumptech.glide.Glide
 import com.google.firebase.database.FirebaseDatabase
 import com.nguyendevs.ecolens.activities.CameraActivity
 import com.nguyendevs.ecolens.databinding.ActivityMainBinding
-import com.nguyendevs.ecolens.fragments.ChatHistoryFragment
-import com.nguyendevs.ecolens.fragments.HistoryFragment
+import com.nguyendevs.ecolens.fragments.chat.ChatHistoryFragment
+import com.nguyendevs.ecolens.fragments.history.HistoryFragment
 import com.nguyendevs.ecolens.handlers.*
 import com.nguyendevs.ecolens.managers.*
 import com.nguyendevs.ecolens.model.LoadingStage
