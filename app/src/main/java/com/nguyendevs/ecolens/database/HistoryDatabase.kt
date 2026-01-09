2bfdcb0174e55c5eb442bb9ec328e3424b0241a5
Nguyên Devs <144598460+NguyenDevs@users.noreply.github.com>
2026-01-09 15:28:09 +0700

    Update HistoryDatabase.kt

--- app/src/main/java/com/nguyendevs/ecolens/database/HistoryDatabase.kt
+++ app/src/main/java/com/nguyendevs/ecolens/database/HistoryDatabase.kt
@@ -5,9 +5,9 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
-import com.nguyendevs.ecolens.model.ChatMessage
-import com.nguyendevs.ecolens.model.ChatSession
-import com.nguyendevs.ecolens.model.HistoryEntry
+import com.nguyendevs.ecolens.model.chat.ChatMessage
+import com.nguyendevs.ecolens.model.chat.ChatSession
+import com.nguyendevs.ecolens.model.history.HistoryEntry
 
 /**
  * Room Database chính của ứng dụng EcoLens
