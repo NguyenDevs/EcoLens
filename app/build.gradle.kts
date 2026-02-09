041aac227a0a9e07daf314f284e5ee21655e2e60
Nguyên Devs <144598460+NguyenDevs@users.noreply.github.com>
2026-02-09 15:39:40 +0700

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -187,4 +187,8 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    // Apache POI for Docx/Xlsx Export
+    implementation("org.apache.poi:poi:5.2.3")
+    implementation("org.apache.poi:poi-ooxml:5.2.3")
 }
