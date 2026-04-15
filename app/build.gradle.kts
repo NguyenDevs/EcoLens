78f974ac715e2118d459c29b3bba7bc76305039e
Nguyên Devs <144598460+NguyenDevs@users.noreply.github.com>
2026-04-16 03:14:28 +0700

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,7 +49,8 @@ android {
         ndk {
             abiFilters += listOf(
                 //"armeabi-v7a",
-                "arm64-v8a"
+                "arm64-v8a",
+                "x86_64"
             )
         }
     }
